96e2a91aea31c73c01cb8666bc67653160ae3a39
geyan <2277087813@qq.com>
2022-01-06 22:58:20 +0800

    注册文件生成

--- router-compiler/src/main/java/com/mars/infra/router/compiler/UriProcessor.kt
+++ router-compiler/src/main/java/com/mars/infra/router/compiler/UriProcessor.kt
@@ -6,32 +6,44 @@ import javax.annotation.processing.RoundEnvironment
 import javax.lang.model.element.Element
 import javax.lang.model.element.Modifier
 import javax.lang.model.element.TypeElement
-import javax.tools.Diagnostic
+import kotlin.random.Random
 
 /**
  * Created by JohnnySwordMan on 2022/1/5
  */
 class UriProcessor : BaseProcessor() {
 
-    override fun process(
-        annotations: MutableSet<out TypeElement>,
-        env: RoundEnvironment
-    ): Boolean {
+    override fun process(annotations: MutableSet<out TypeElement>, env: RoundEnvironment): Boolean {
+        // 放在forEach之前创建，不能在forEach里面创建，否则始终只有第一个代码块创建
+        val codeBlockBuilder = CodeBlock.builder()
+        var hash: String? = null
         env.getElementsAnnotatedWith(RouterUri::class.java)
             .filter { it.kind.isClass }
             .forEach { element ->
-//                mMessager?.printMessage(
-//                    Diagnostic.Kind.WARNING,
-//                    "UriProcessor element = ${element.simpleName}"
-//                )
                 println("UriProcessor---element = ${element.simpleName}")
                 if (isSubType(element, ACTIVITY)) {
-//                    mMessager?.printMessage(Diagnostic.Kind.WARNING, "$element 是Activity的子类")
                     println("UriProcessor---$element 是Activity的子类")
-                    buildUriAnnotationInitClass(element)
+                    val uri = element.getAnnotation(RouterUri::class.java)
+                    if (hash == null) {
+                        // 不为空才创建，否则会出现一个模块存在多个文件
+                        hash = Random.nextInt(10000).toString()
+                    }
+                    codeBlockBuilder.addStatement(
+                        "handler.register(\$S, \$S, \$S)",
+                        uri.module,
+                        uri.path,
+                        element
+                    )
+
+
                 }
             }
-        return false
+        if (hash == null) {
+            hash = "996"
+        }
+        // 不能重复创建Java文件，因此这个需要放在forEach之后
+        buildUriAnnotationInitClass(codeBlockBuilder.build(), hash)
+        return true
     }
 
     /**
@@ -42,7 +54,7 @@ class UriProcessor : BaseProcessor() {
      *      }
      * }
      */
-    private fun buildUriAnnotationInitClass(element: Element) {
+    private fun buildUriAnnotationInitClass(codeBlock: CodeBlock, hash: String? = null) {
         val simpleName = "UriAnnotationInit"
         val superInterfaceName = URI_ANNOTATION_INIT_CLASS
 
@@ -50,11 +62,12 @@ class UriProcessor : BaseProcessor() {
             MethodSpec.methodBuilder(URI_ANNOTATION_INIT_METHOD)
                 .addModifiers(Modifier.PUBLIC)
                 .addParameter(ClassName.get(typeElement(URI_HANDLER_CLASS)), "handler")
+                .addCode(codeBlock)
                 .returns(TypeName.VOID)
                 .build()
 
         val typeSpec =
-            TypeSpec.classBuilder("${simpleName}_123456")
+            TypeSpec.classBuilder("${simpleName}_${hash}")
                 .addModifiers(Modifier.PUBLIC)
                 .addSuperinterface(ClassName.get(typeElement(superInterfaceName)))  // 如果typeElement方法崩溃，需要检查是否对应的className打到apk中
                 .addMethod(methodSpec)
--- router/src/main/java/com/mars/infra/router/UriHandler.kt
+++ router/src/main/java/com/mars/infra/router/UriHandler.kt
@@ -4,4 +4,8 @@ package com.mars.infra.router
  * Created by JohnnySwordMan on 2022/1/5
  */
 class UriHandler {
+
+    fun register(module: String?, path: String?, className: String) {
+
+    }
 }
